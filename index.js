8f54783b2dd5b50765037d9e10f1de8cc3e99d40
Kyle Robinson Young <kyle@dontkry.com>
2016-06-22 20:11:26 -0700

    use on-load@2.1.0

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 var yo = require('yo-yo')
 var assign = require('object-assign')
 var document = require('global/document')
+var onload = require('on-load')
 
 module.exports = function modalElement (contents) {
   var modal = null
   var el = render(contents)
-  bindMouseDown()
   function modalShow (newContents) {
     if (newContents) contents = newContents
     el = yo.update(el, render(contents))
-    bindMouseDown()
   }
   function modalHide () {
     el = yo.update(el, document.createTextNode(''))
-    unbindMouseDown()
   }
   function modalToggle (newContents) {
     if (!el || el.nodeName === '#text') {
@@ -21,16 +19,6 @@ module.exports = function modalElement (contents) {
     } else {
       modalHide()
     }
-  }
-  function bindMouseDown () {
-    if (el && el.dataset && !el.dataset.mouseDownIsBound) {
-      document.addEventListener('mousedown', didClickOut, false)
-      el.dataset.mouseDownIsBound = true
-    }
-  }
-  function unbindMouseDown () {
-    document.removeEventListener('mousedown', didClickOut, false)
-    if (el && el.dataset) delete el.dataset.mouseDownIsBound
   }
   function didClickOut (e) {
     var source = e.target
@@ -50,6 +38,11 @@ module.exports = function modalElement (contents) {
   function render (contents) {
     modal = yo`<div class="modal">${contents}</div>`
     var overlay = yo`<div class="modal-overlay">${modal}</div>`
+    onload(overlay, function () {
+      document.addEventListener('mousedown', didClickOut, false)
+    }, function () {
+      document.removeEventListener('mousedown', didClickOut, false)
+    })
     assign(overlay.style, {
       position: 'fixed',
       height: '100%',
